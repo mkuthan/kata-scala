e4120b11a08645c42330f5cde5e2126890323fec
Marcin Kuthan <marcin.kuthan@gmail.com>
2013-07-12 22:34:39 +0200

    Combination cont.

--- src/main/scala/kata/s99/P26.scala
+++ src/main/scala/kata/s99/P26.scala
@@ -1,12 +1,14 @@
 package kata.s99
 
 object P26 {
-  
+
   /**
    * xs.combinations(k).toList
    */
-  def combinations(k: Int, xs: List[String]): List[List[String]] = {
-    Nil
+  def combinations(k: Int, xs: List[String]): List[List[String]] = (k, xs) match {
+    case (0, _) => List(List())
+    case (_, Nil) => List()
+    case _ => (combinations(k - 1, xs.tail) map (xs.head :: _)) ::: combinations(k, xs.tail)
   }
-    
+
 }
